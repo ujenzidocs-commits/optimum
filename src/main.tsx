74bb4aa9ee786bf10bb1dfd4c1b0dcb2c5b8619a
kirukijohn45 <kirukijohnmark123@gmail.com>
2026-05-22 18:55:21 +0000

    Updated dependencies and project files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// @ts-ignore: CSS imported as a side effect without type declarations
 import "./index.css";
 import App from "./App";
 
